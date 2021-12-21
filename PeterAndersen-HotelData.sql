40f06289c3228c7fc32b17ff03e5e98c160953e3
PeterAleksander <75710472+PeterAleksander@users.noreply.github.com>
2021-12-21 10:34:38 -0600

    Update PeterAndersen-HotelData.sql

--- PeterAndersen-HotelData.sql
+++ PeterAndersen-HotelData.sql
@@ -1,3 +1,5 @@
+
+--Insert Self into DB
 INSERT INTO Guest
 VALUES
 ('Peter','Andersen','3600 Jeanetta St','Houston','TX','77063','832 348-9976');
@@ -6,5 +8,6 @@ INSERT INTO Reservation
 VALUES
 (305,111,1,0,'2023-08-23','2023-09-16',349.98);
 
+--DELETE Jeremiah
 DELETE FROM Reservation WHERE GuestId = 106;
 DELETE FROM Guest WHERE GuestId = 106;
